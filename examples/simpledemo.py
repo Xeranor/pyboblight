77d08e70a4cda24ccaa562480786b6e60c4bfcb8
Jonas Schulz <jonas.schulz.12@gmail.com>
2016-04-18 19:48:26 +0200

    Fixed example

--- examples/simpledemo.py
+++ examples/simpledemo.py
@@ -3,7 +3,7 @@ import time
 import random
 
 if __name__=='__main__':
-    client=pyboblight.BobCLient()
+    client=pyboblight.BobClient()
 
     #print light information
     print client.lights
@@ -14,7 +14,8 @@ if __name__=='__main__':
     while time.time()<stop:
         time.sleep(0.1)
         #prepare the light color changes
-        for name,light in client.lights.iteritems():
-            light.set_color(random.randint(0,255),random.randint(0,255),random.randint(0,255))
+        for side in client.lights:
+            for light in client[side]:
+                light.set_color(random.randint(0,255),random.randint(0,255),random.randint(0,255))
         #tell the client to update the current light color state on the server
         client.update()
